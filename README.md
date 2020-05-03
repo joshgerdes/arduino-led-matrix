8093433f24ca2f0d4d9d2020d1b6379f5579c683
Josh Gerdes <joshgerdes@gmail.com>
2020-05-03 10:07:48 -0500

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# arduino-led-matrix
- An Arduino powered 16x16 LED Matrix using the FastLED library to drive a WS2812B LED array.
+# Arduino LED Matrix
+ 
+ An Arduino powered 16x16 LED Matrix using the [FastLED](http://fastled.io/) library to drive a WS2812B LED array.
+
+## Wiring Diagram
+
+![Fritzing diagram sketch](resources/wiring-diagram_bb.png)
+
+
+## 3D Models
+
+All models were created in [Tinkercad](https://www.tinkercad.com/) and exported in STL format.
+
+- [Matrix Grid 1/4th - Side A](resources/models/LED_Matrix_4th_SideA.stl)
+- [Matrix Grid 1/4th - Side B](resources/models/LED_Matrix_4th_SideB.stl)
+- [Arduino Holder Base](resources/models/Nano_Holder_Base.stl)
+- [Arduino Holder Lid](resources/models/Nano_Holder_Lid.stl)
+  
+## Materials List
+
+| Part | Qty |   |
+|---|---|---|
+| Arduino Nano | 1 | https://www.amazon.com/gp/product/B07XVYBR9Y |
+| WS2812B Strip Light - 60 LEDs, 5M | 1  | https://www.ebay.com/itm//264037239475 |
+| 3 PIN Solderless LED Cable Connector For WS2812B WS2811 LED Strip Lamp 15cm  | 16  | https://www.ebay.com/itm/153661361028 |
+| 12mm SPST NO Reset Switch Push Button | 2  | https://www.amazon.com/gp/product/B075VBWFM6 |
+| Breadboard Jumper Wires | 7 | https://www.amazon.com/dp/B01EV70C78 |
+| Black Wood Shadow Box - 12" x 12" | 1 | https://www.hobbylobby.com/Home-Decor-Frames/Frames-Photo-Albums/Shadow-Boxes-Display-Cases/Black-Wood-Shadow-Box---12%22-x-12%22/p/39376 |
+| Creatology poser board plastic 22" x 28" | 1 | https://www.michaels.com/pacon-plastic-poster-board-clear/10378153.html |
+| Elmers Foamboard 950109 20" x 30" x 3/16" | 1 | https://www.michaels.com/elmers-foam-board-white/10110205.html |
+| Command Strip Mini Hooks (17006CLR-ES) | 2 | https://www.amazon.com/dp/B0084M67P8 |
+| Electrical Tape | 1 | https://www.amazon.com/dp/B00004WCCL |
+
+## Software & Other Resources
+
+- Visual Studio Code - https://code.visualstudio.com/
+- Arduino IDE - https://www.arduino.cc/en/Main/Software
+- Ultimaker Cura - https://ultimaker.com/software/ultimaker-cura
+- LCD Image Convertor - https://www.riuson.com/lcd-image-converter
+- GIMP - https://www.gimp.org/
+- Sprite Image Resources - https://www.spriters-resource.com/
---
+++ arduino-led-matrix.ino
@@ -0,0 +1,160 @@
+#include <avr/pgmspace.h>
+#include "src/libraries/FastLED/FastLED.h"
+#include "src/bitmaps/digdug.h"
+#include "src/bitmaps/frogger.h"
+#include "src/bitmaps/galaga.h"
+#include "src/bitmaps/pacman.h"
+#include "src/bitmaps/smb.h"
+#include "src/bitmaps/qbert.h"
+
+#define DATA_PIN        4
+#define BUTTON_PIN      3
+#define BUTTONTWO_PIN   2
+#define NUM_LEDS        256
+#define BRIGHTNESS      16
+#define LED_TYPE        WS2812B
+#define COLOR_ORDER     GRB
+#define COLS            16
+
+const int MAX_SELECTION = 4;
+const int MAX_SELECTION_TWO[] = { 0, 3, 3, 4 };
+ 
+CRGB leds[NUM_LEDS];
+
+volatile int selection = 0;
+volatile int selectionTwo = 0;
+long lastDebounceTime = 0; 
+long lastDebounceTimeTwo = 0;  
+long debounce = 100;         
+
+void setup() {
+  delay(3000); // power-up safety delay
+  Serial.begin(9600);
+  
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.clear();
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonToggled, RISING);
+  pinMode(BUTTONTWO_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTONTWO_PIN), buttonTwoToggled, RISING);
+
+  selection = 1;
+  selectionTwo = 0;
+  
+  Serial.print("Setup Complete");
+}
+
+void drawBitmap(const long* bitmap) {
+  FastLED.clear();
+  bool LTR = true;
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (LTR) {
+      for (int j = 0; j < COLS; j++) {
+        leds[i + j] = pgm_read_dword(&(bitmap[i + j]));
+      }
+    } else {
+      for (int j = COLS - 1; j > 0; j--) {
+        leds[i + COLS - j] = pgm_read_dword(&(bitmap[i + j - 1]));
+      }
+    }
+
+    i = i + COLS - 1;
+    LTR = !LTR;
+  }
+
+  FastLED.show();
+}
+
+void drawAnimation(const long* bitmaps, int s, int delayTime) {
+  for (int i = 0; i < s; i++) {
+    drawBitmap(bitmaps[i]);
+    delay(delayTime);
+  }
+}
+
+void buttonToggled() {
+  if (millis() - lastDebounceTime > debounce) {
+    selectionTwo = 0;
+    selection++;
+    if (selection > MAX_SELECTION)  selection = 1;
+    lastDebounceTime = millis();
+  }
+}
+
+void buttonTwoToggled() {
+  if (millis() - lastDebounceTimeTwo > debounce) {
+    selectionTwo++;
+    if (selectionTwo > MAX_SELECTION_TWO[selection])  selectionTwo = 0;
+    lastDebounceTimeTwo = millis();
+  }
+}
+
+void galagaLoop() {
+  switch (selectionTwo) {
+    case 1:
+      drawAnimation(GalagaBoss, GalagaBossSize, 500);
+      break;
+    case 2:
+      drawAnimation(GalagaGoei, GalagaGoeiSize, 500);
+      break;
+    case 3:
+      drawAnimation(GalagaZako, GalagaZakoSize, 500);
+      break;
+    default:
+      drawBitmap(GalagaShip);
+      break;
+  }
+}
+
+void digDugLoop() {
+  switch (selectionTwo) {
+    case 1:
+      drawAnimation(DigDugTaizoShovel, DigDugTaizoShovelSize, 500);
+      break;
+    case 2:
+      drawAnimation(DigDugPooka, DigDugPookaSize, 500);
+      break;
+    case 3:
+      drawAnimation(DigDugFygar, DigDugFygarSize, 500);
+      break;
+    default:
+      drawBitmap(DigDugTaizoShovel01);
+      break;
+  }
+}
+
+void miscLoop() {
+  switch (selectionTwo) {
+    case 1:
+      drawAnimation(Frogger, FroggerSize, 500);
+      break;
+    case 2:
+      drawAnimation(Qbert, QbertSize, 500);
+      break;
+    case 3:
+      drawAnimation(PacManMsPac, PacManMsPacSize, 100);
+      break;
+    case 4:
+      drawAnimation(PacManBlinky, PacManBlinkySize, 500);
+      break;
+    default:
+      drawBitmap(SMBMario01);
+      break;
+  }
+}
+
+void loop() {
+  switch (selection) {
+    case 1:
+      galagaLoop();
+      break;
+    case 2:
+      digDugLoop();
+      break;
+    case 3:
+      miscLoop();
+      break;
+  }
+}
